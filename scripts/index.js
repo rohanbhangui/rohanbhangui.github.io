ac36107372a33f0b036fec04306414fe3379561e
rohanbhangui <bhanguirohan@gmail.com>
2014-04-19 21:28:00 -0400

    modal added and finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $(".shots img").on("click", function () {
     console.log($(this).attr("src"));
-    //$("body").prepend("<div class='modal-container'></div>");
+    $("body").prepend("<div class='modal-container'><img src='" + $(this).attr("src") + "' /></div>");
+    $(".modal-container").fadeIn();
+  });
+
+  $("body").on('click','.modal-container', function(e) {
+    if (!$(e.target).is('img')) {
+      $(".modal-container").fadeOut();
+    }
   });
 });
