6ec712fbf8d0fd49b300c66a89374d38e2dd5081
rohanbhangui <bhanguirohan@gmail.com>
2014-04-19 20:36:32 -0400

    added beginnings of modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-
-  
-
+  $(".shots img").on("click", function () {
+    console.log($(this).attr("src"));
+    //$("body").prepend("<div class='modal-container'></div>");
+  });
 });
