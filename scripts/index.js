89ad7cef66fa6699779fac3ad9f862860072e389
rohanbhangui <bhanguirohan@gmail.com>
2014-04-19 22:37:52 -0400

    modal fix final

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
+  
+  $("body").prepend("<div class='modal-container'></div>");
+
   $(".shots img").on("click", function () {
-    console.log($(this).attr("src"));
-    $("body").prepend("<div class='modal-container'><img src='" + $(this).attr("src") + "' /></div>");
+    $(".modal-container").html("<img src='" + $(this).attr("src") + "' />");
+    $("body").css("overflow", "hidden");
+    $(".modal-container").css("overflow", "auto");
     $(".modal-container").fadeIn();
   });
 
   $("body").on('click','.modal-container', function(e) {
     if (!$(e.target).is('img')) {
+      $(".modal-container").css("overflow", "hidden");
+      $("body").css("overflow", "auto");
       $(".modal-container").fadeOut();
     }
   });
