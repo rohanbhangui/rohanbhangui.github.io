73e2cb6bcc8eb9954d39aabbb0626797bfbba8de
rohanbhangui <bhanguirohan@gmail.com>
2014-04-27 17:16:50 -0400

    deeplink commenting out

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
   });
   
   $(".modal-backbutton").on("click", function(e) {
-    deepLink(0, "mainpage", "/");
+    //deepLink(0, "mainpage", "/");
     $(".modal").css("overflow", "hidden");
     $("body").css("overflow", "auto");
     $(".modal").fadeOut();
