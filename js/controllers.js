de4512dc29088e3602e67022e20f0b1ff36ff630
rohanbhangui <bhanguirohan@gmail.com>
2014-12-07 14:03:55 -0500

    fixed side nav

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ var portfolio = [
     "id": '1',
     "name": "flo",
     "permalink": "/frontend/",
+    "target": "_blank",
     "title": "Flo UI Frontend Framework",
     "content": "Co-authored a frontend framework designed for fast prototyping of flat web design"
   },
@@ -10,6 +11,7 @@ var portfolio = [
     "id": '2',
     "name": "gethighered",
     "permalink": "http://gethighered.ca",
+    "target": "_blank",
     "title": "Get Highered Redesign",
     "content": "A Canadian student jobs startup that needed a site redesign. I was incharge of design and front end development"
   },
@@ -17,6 +19,7 @@ var portfolio = [
     "id": '3',
     "name": "line-icons",
     "permalink": "/portfolio/line-icons/",
+    "target": "_self",
     "title": "Line Icons",
     "content": "An icon font created for use in webapps and mobile web programming"
   },
@@ -24,6 +27,7 @@ var portfolio = [
     "id": '4',
     "name": "wikipedia",
     "permalink": "/portfolio/wikipedia",
+    "target": "_self",
     "title": "Wikipedia Redesign",
     "content": "Concept redesign for some of wikipedia's visual assets and certain parts of their site"
   },
@@ -31,6 +35,7 @@ var portfolio = [
     "id": '5',
     "name": "offthewallmedia",
     "permalink": "/portfolio/offthewallmedia/",
+    "target": "_self",
     "title": "OffTheWall Media Redesign",
     "content": "My process for a concept logo for OffTheWall Media"
   },
@@ -38,13 +43,15 @@ var portfolio = [
     "id": '6',
     "name": "closet-theory",
     "permalink": "/portfolio/closet-theory/",
-    "title": "Closet theory Redesign",
+    "target": "_self",
+    "title": "Closet Theory Redesign",
     "content": "Redesign for a boutique custom closet organizing consultancy"
   },
   {
     "id": '7',
     "name": "veloria",
     "permalink": "/portfolio/veloria/",
+    "target": "_self",
     "title": "Veloria Site Designs",
     "content": "Concept site designs for concert/party planning business"
   }
