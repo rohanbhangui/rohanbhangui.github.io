1ce8d432dad41fdfebff837f68b08f6e00445127
rohanbhangui <bhanguirohan@gmail.com>
2016-03-08 14:04:00 -0500

    commented out reveal code for preview shots in writhem

--- js/index.js
+++ js/index.js
@@ -20,4 +20,34 @@ jQuery(function($){
 		    $(".project-subtitle").addClass("animated fadeInUp");
 		});
 	});
+
+	// var shot3Offset = $(".writhem-radio #shot-3").offset();
+	// $(window).scroll(function() {
+
+	// 	console.log($(window).scrollTop());
+	// 	if($(this).scrollTop() >= shot3Offset.top) {
+	// 		$( ".writhem-radio #shot-3 .row .swipebox").first().show( "slow", function showNext() {
+	// 			$( this ).next( ".writhem-radio #shot-3 .row .swipebox" ).show( "fast", showNext );
+	// 		});
+	// 	}
+	// });
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
